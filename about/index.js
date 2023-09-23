9f66c6453e70661c24068d5069ed774bfcad651c
Aswin S <aswinsnair028@gmail.com>
2023-09-23 20:21:36 +0530

    new about

--- about/index.js
+++ about/index.js
@@ -1,84 +1,138 @@
-/*
- * Star Wars opening crawl from 1977
- * 
- * I freaking love Star Wars, but could not find
- * a web version of the original opening crawl from 1977.
- * So I created this one.
- *
- * I wrote an article where I explain how this works:
- * http://timpietrusky.com/star-wars-opening-crawl-from-1977
- * 
- * Watch the Start Wars opening crawl on YouTube.
- * https://www.youtube.com/watch?v=7jK-jZo6xjY
- * 
- * Stuff I used:
- * - CSS (animation, transform)
- * - HTML audio (the opening theme)
- * - SVG (the Star Wars logo from wikimedia.org)
- *   http://commons.wikimedia.org/wiki/File:Star_Wars_Logo.svg
- * - JavaScript (to sync the animation/audio)
- *
- * Thanks to Craig Buckler for his amazing article 
- * which helped me to create this remake of the Star Wars opening crawl. 
- * http://www.sitepoint.com/css3-starwars-scrolling-text/ 
- *
- * Sound copyright by The Walt Disney Company.
- * 
- *
- * 2013 by Tim Pietrusky
- * timpietrusky.com
- * 
- */
-StarWars = (function () {
 
-  /* 
-   * Constructor
-   */
-  class StarWars {
-    constructor(args) {
-      // Context wrapper
-      this.el = $(args.el);
-
-      // Audio to play the opening crawl
-      this.audio = this.el.find('audio').get(0);
-
-      // Start the animation
-      this.start = this.el.find('.start');
-
-      // The animation wrapper
-      this.animation = this.el.find('.animation');
-
-      // Remove animation and shows the start screen
-      this.reset();
-
-      // Start the animation on click
-      this.start.bind('click', $.proxy(function () {
-        this.start.hide();
-        this.audio.play();
-        this.el.append(this.animation);
-      }, this));
-
-      // Reset the animation and shows the start screen
-      $(this.audio).bind('ended', $.proxy(function () {
-        this.audio.currentTime = 0;
-        this.reset();
-      }, this));
-    }
+var NUM_PARTICLES = ( ( ROWS = isMobile ? 350 : 200 ) * ( COLS = isMobile ?  150 : 400 ) ),
+    THICKNESS = Math.pow( 80, 2 ),
+    SPACING = 3,
+    MARGIN = 100,
+    COLOR = 220,
+    DRAG = 0.95,
+    EASE = 0.25,
+    
     /*
-       * Resets the animation and shows the start screen.
-       */
-    reset() {
-      this.start.show();
-      this.cloned = this.animation.clone(true);
-      this.animation.remove();
-      this.animation = this.cloned;
-    }
+    
+    used for sine approximation, but Math.sin in Chrome is still fast enough :)http://jsperf.com/math-sin-vs-sine-approximation
+
+    B = 4 / Math.PI,
+    C = -4 / Math.pow( Math.PI, 2 ),
+    P = 0.225,
+
+    */
+
+    container,
+    particle,
+    canvas,
+    mouse,
+    stats,
+    list,
+    ctx,
+    tog,
+    man,
+    dx, dy,
+    mx, my,
+    d, t, f,
+    a, b,
+    i, n,
+    w, h,
+    p, s,
+    r, c
+    ;
+
+particle = {
+  vx: 0,
+  vy: 0,
+  x: 0,
+  y: 0
+};
+
+function init() {
+
+  container = document.getElementById( 'container' );
+  canvas = document.createElement( 'canvas' );
+  
+  ctx = canvas.getContext( '2d' );
+  man = false;
+  tog = true;
+  
+  list = [];
+  
+  w = canvas.width = COLS * SPACING + MARGIN * 2;
+  h = canvas.height = ROWS * SPACING + MARGIN * 2;
+  
+  container.style.marginLeft = Math.round( w * -0.5 ) + 'px';
+  container.style.marginTop = Math.round( h * -0.5 ) + 'px';
+  
+  for ( i = 0; i < NUM_PARTICLES; i++ ) {
+    
+    p = Object.create( particle );
+    p.x = p.ox = MARGIN + SPACING * ( i % COLS );
+    p.y = p.oy = MARGIN + SPACING * Math.floor( i / COLS );
+    
+    list[i] = p;
   }
 
+  container.addEventListener( 'mousemove', function(e) {
 
-  return StarWars;
-})();
+    bounds = container.getBoundingClientRect();
+    mx = e.clientX - bounds.left;
+    my = e.clientY - bounds.top;
+    man = true;
+    
+  });
+  
+  if ( typeof Stats === 'function' ) {
+    document.body.appendChild( ( stats = new Stats() ).domElement );
+  }
+  
+  container.appendChild( canvas );
+}
 
-new StarWars({
-  el: '.starwars'
-});
+function step() {
+
+  if ( stats ) stats.begin();
+
+  if ( tog = !tog ) {
+
+    if ( !man ) {
+
+      t = +new Date() * 0.001;
+      mx = w * 0.5 + ( Math.cos( t * 2.1 ) * Math.cos( t * 0.9 ) * w * 0.45 );
+      my = h * 0.5 + ( Math.sin( t * 3.2 ) * Math.tan( Math.sin( t * 0.8 ) ) * h * 0.45 );
+    }
+      
+    for ( i = 0; i < NUM_PARTICLES; i++ ) {
+      
+      p = list[i];
+      
+      d = ( dx = mx - p.x ) * dx + ( dy = my - p.y ) * dy;
+      f = -THICKNESS / d;
+
+      if ( d < THICKNESS ) {
+        t = Math.atan2( dy, dx );
+        p.vx += f * Math.cos(t);
+        p.vy += f * Math.sin(t);
+      }
+
+      p.x += ( p.vx *= DRAG ) + (p.ox - p.x) * EASE;
+      p.y += ( p.vy *= DRAG ) + (p.oy - p.y) * EASE;
+
+    }
+
+  } else {
+
+    b = ( a = ctx.createImageData( w, h ) ).data;
+
+    for ( i = 0; i < NUM_PARTICLES; i++ ) {
+
+      p = list[i];
+      b[n = ( ~~p.x + ( ~~p.y * w ) ) * 4] = b[n+1] = b[n+2] = COLOR, b[n+3] = 255;
+    }
+
+    ctx.putImageData( a, 0, 0 );
+  }
+
+  if ( stats ) stats.end();
+
+  requestAnimationFrame( step );
+}
+
+init();
+step();
